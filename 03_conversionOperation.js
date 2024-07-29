e08b7feba78dfc6671656a57ae1f618d18cff0f9
Ujjwal Tiwari <112298492+UjjwalTiwari25@users.noreply.github.com>
2024-07-29 13:18:43 +0530

    Comparision

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -1,74 +1,91 @@
-// let score="33abc";
-let score=null
-console.log(typeof score);
-console.log(typeof(score));
-let valueInNumber = Number(score)
-console.log(valueInNumber);
-let score = "hitesh"
+// // let score="33abc";
+// let score=null
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// let score = "hitesh"
 
-//console.log(typeof score);
-//console.log(typeof(score));
+// //console.log(typeof score);
+// //console.log(typeof(score));
 
-let valueInNumber = Number(score)
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// //console.log(typeof valueInNumber);
+// //console.log(valueInNumber);
 
 
-// "33" => 33
-// "33abc" => NaN
-//  true => 1; false => 0
+// // "33" => 33
+// // "33abc" => NaN
+// //  true => 1; false => 0
 
-let isLoggedIn = "hitesh"
+// let isLoggedIn = "hitesh"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-//  console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// //  console.log(booleanIsLoggedIn);
 
-// 1 => true; 0 => false
-// "" => false
-// "hitesh" => true
+// // 1 => true; 0 => false
+// // "" => false
+// // "hitesh" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// // console.log(stringNumber);
+// // console.log(typeof stringNumber);
 
-// *********************** Operations ***********************
+// // *********************** Operations ***********************
 
-let value = 3
-let negValue = -value
-// console.log(negValue);
+// let value = 3
+// let negValue = -value
+// // console.log(negValue);
 
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**3);
-// console.log(2/3);
-// console.log(2%3);
+// // console.log(2+2);
+// // console.log(2-2);
+// // console.log(2*2);
+// // console.log(2**3);
+// // console.log(2/3);
+// // console.log(2%3);
 
-let str1 = "hello"
-let str2 = " hitesh"
+// let str1 = "hello"
+// let str2 = " hitesh"
 
-let str3 = str1 + str2
-// console.log(str3);
+// let str3 = str1 + str2
+// // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// // console.log("1" + 2);
+// // console.log(1 + "2");
+// // console.log("1" + 2 + 2);
+// // console.log(1 + 2 + "2");
 
-// console.log( (3 + 4) * 5 % 3);
+// // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+// // console.log(+true);
+// // console.log(+"");
 
-let num1, num2, num3
+// let num1, num2, num3
 
-num1 = num2 = num3 = 2 + 2
+// num1 = num2 = num3 = 2 + 2
 
-let gameCounter = 100
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+// // link to study
+// // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+let gameCounter=100;
+// gameCounter++;
 ++gameCounter;
 console.log(gameCounter);
 
-// link to study
-// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+let a = 5;
+let b = ++a;  // a is incremented to 6, then b is assigned the value of a, so b is 6
+
+let x = 5;
+let y = --x;  // x is decremented to 4, then y is assigned the value of x, so y is 4
+
+
+let a = 5;
+let b = a++;  // b is assigned the value of a (5), then a is incremented to 6
+
+let x = 5;
+let y = x--;  // y is assigned the value of x (5), then x is decremented to 4
