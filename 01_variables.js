db1e3b52f7e2cdf8bb3787453824e42e8bc615d5
Ujjwal Tiwari <112298492+UjjwalTiwari25@users.noreply.github.com>
2024-07-29 08:03:47 +0530

    Conversion Operation

---
+++ 01_variables.js
@@ -0,0 +1,10 @@
+console.log("Hello Ujjwal");
+// const accountId=5487;
+let userName="ujjwal";
+var address="Gkp";
+console.log(address);
+let accountId=789;
+console.log(accountId);
+/*Prefer not to use "var" because of problem of block scope and functional scope*/
+
+console.table([userName,address,accountId]);
